fa0f1a92ab29c0b2e2c22eafca3ca04a7a7f8b88
erwannbr <erwann.briot@efrei.net>
2026-02-23 17:17:30 +0100

    commut the sql file

---
+++ 1_creation.sql
@@ -0,0 +1,55 @@
+
+CREATE TABLE Houses (
+    H_name VARCHAR(50),
+    H_motto VARCHAR(50),
+    H_animal VARCHAR(50),
+    PRIMARY KEY (H_name)
+);
+
+CREATE TABLE Teachers (
+    T_ID INT,
+    T_firstname VARCHAR(50),
+    T_lastname VARCHAR(50),
+    PRIMARY KEY (T_ID)
+);
+
+CREATE TABLE Students (
+    S_ID INT,
+    S_firstname VARCHAR(50),
+    S_lastname VARCHAR(50),
+    H_name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (S_ID),
+    FOREIGN KEY (H_name) REFERENCES Houses (H_name)
+);
+
+CREATE TABLE Courses (
+    C_ID VARCHAR(50),
+    C_title VARCHAR(50),
+    T_ID INT NOT NULL,
+    PRIMARY KEY (C_ID),
+    FOREIGN KEY (T_ID) REFERENCES Teachers (T_ID)
+);
+
+CREATE TABLE takes (
+    S_ID INT,
+    C_ID VARCHAR(50),
+    grade INT,
+    PRIMARY KEY (S_ID , C_ID),
+    FOREIGN KEY (S_ID) REFERENCES Students (S_ID),
+    FOREIGN KEY (C_ID) REFERENCES Courses (C_ID)
+);
+
+CREATE TABLE assigns (
+    H_name VARCHAR(50),
+    T_ID INT,
+    points INT,
+    PRIMARY KEY (H_name , T_ID),
+    FOREIGN KEY (H_name) REFERENCES Houses (H_name),
+    FOREIGN KEY (T_ID) REFERENCES Teachers (T_ID)
+);
+
+show tables;
+describe Courses;
+describe Houses;
+describe Teachers;
+describe Students;
